01d509dbead9659e1f290504f435046e125fef51
Sara Jo Helba <sarahelba@Saras-MacBook-Pro.local>
2017-08-13 10:19:48 -0400

    added extra solution possibilities in comments

--- models/user.js
+++ models/user.js
@@ -2,9 +2,46 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+     first: {
+       type: Sequelize.STRING
+     },
+     last: {
+      type: Sequelize.STRING
+     },
+     age: {
+      type: Sequelize.INTEGER,
+      validate: {
+        min: 18
+      }
+     },
+     email: {
+      type: Sequelize.STRING,
+      allowNull: false
+     },
+     bio: {
+      type: Sequelize.TEXT
+     }
 }, {
-// ...AND HERE
+  getterMethods: {
+    fullName: function() {
+      return this.first + ' ' + this.last;
+    }
+  }
 });
+
+User.prototype.haveBirthday = function() {
+  return this.increment('age', {by: 1});
+}
+
+// Note other possibilities:
+
+// User.prototype.haveBirthday = function() {
+//   this.age++;
+//   return this.save();
+// }
+
+// User.prototype.haveBirthday = function() {
+//   return this.update({age: this.age + 1});
+// }
 
 module.exports = User;
